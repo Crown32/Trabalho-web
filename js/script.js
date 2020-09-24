c32ec6f11f92d10a124f1aec8598f602e5714307
Lucas Eleutério <leleuteiobueno@gmail.com>
2020-09-24 01:56:12 -0300

    almost finished site

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-  $(document).ready(function () {
-    $(".minus").hide();
-  });
+//Script para acordeão
+$(document).ready(function () {
+  for (let index = 0; index < 5; index++) {
+    $(`.minus${index}`).hide();
+  }
+});
 
-  function check(type) {
-      const checkbox=document.getElementById(`check${type}`).value;
-      console.log(checkbox);
-    if (checkbox) {
-      $(".plus").hide();
-      $(".minus").show();
-      console.log("Tô marcado");
-    } else {
-      console.log("Tô desmarcado");
-      $(".plus").show();
-      $(".minus").hide();
-    }
+function check(type) {
+  const checkbox = document.getElementById(`check${type}`).checked;
+  console.log(checkbox);
+  if (checkbox==false) {
+    $(`check${type}`).prop("checked", true)
+    $(`.plus${type}`).hide();
+    $(`.minus${type}`).show();
+  } else {
+    $(`check${type}`).prop("checked", false)
+    $(`.plus${type}`).show();
+    $(`.minus${type}`).hide();
+  }
 }
-
