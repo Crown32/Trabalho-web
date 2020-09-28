95f6d7b1ff668a6f95e375eafa2f48caa3fcef6a
Lucas Eleutério <leleuteiobueno@gmail.com>
2020-09-28 09:52:10 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+//Header scolling change
+// window.onscroll = () => {
+//   let top = window.scrollY;
+  
+//   if (top <= 300) {
+//     $(".navbar").animate({
+//       backgroundColor: "none"
+//     });
+//   } else {
+//     $(".navbar").animate({
+//       fontSize: 15,
+//       backgroundColor: jQuery.Color("rgba(255, 255, 255, 0.9)"),
+//     });
+//   }
+// };
+
 //Script para acordeão
 $(document).ready(function () {
   for (let index = 0; index < 5; index++) {
@@ -8,12 +24,12 @@ $(document).ready(function () {
 function check(type) {
   const checkbox = document.getElementById(`check${type}`).checked;
   console.log(checkbox);
-  if (checkbox==false) {
-    $(`check${type}`).prop("checked", true)
+  if (checkbox == false) {
+    $(`check${type}`).prop("checked", true);
     $(`.plus${type}`).hide();
     $(`.minus${type}`).show();
   } else {
-    $(`check${type}`).prop("checked", false)
+    $(`check${type}`).prop("checked", false);
     $(`.plus${type}`).show();
     $(`.minus${type}`).hide();
   }
