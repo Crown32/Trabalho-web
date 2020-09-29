7e8ea9475a2ea6f444ccca203956b96c03c79563
Lucas Eleutério <leleuteiobueno@gmail.com>
2020-09-29 15:05:37 -0300

    just need to fix the bug when hover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Navbar scrolling change
-$(document).on('scroll', function (e) {
+$(document).on("scroll", function (e) {
   $(".navbar-down").show();
-  $('.navbar-down').css('opacity', ($(document).scrollTop() / 350));
+  $(".navbar-down").css("opacity", $(document).scrollTop() / 350);
 });
 
 //Script para acordeão
@@ -25,9 +25,71 @@ function check(type) {
 }
 
 //Mosaic js
-    $('#myMosaic').Mosaic({
-      maxRows:10,
-      maxRowHeightPolicy:"crop",
-      maxRowHeight:500,
-      outerMargin:100
+function blured(id, width, height) {
+  $.getJSON("../data/Data.json", (data) => {
+    const dataContent = data.filter((data) => data.id == id);
+
+    $(`#${id}`).fadeOut(250, () => {
+      $(`#${id}`).replaceWith(
+        `<div id="${id}"onmouseleave="normal(this.id)" style="display: flex; justify-content: center; align-items: center; flex-direction: column; width:${width}px; height:${height}px;">
+      <h5>${dataContent[0].title}</h5>
+      <p>${dataContent[0].content}</p>
+    </div>`
+      );
+
+      $(`#${id}`).hide();
+      $(`#${id}`).fadeIn(250);
+
+      $("#myMosaic").Mosaic({
+        maxRows: 10,
+        maxRowHeightPolicy: "crop",
+        maxRowHeight: 500,
+      });
     });
+  });
+}
+
+function normal(id) {
+  $.getJSON("../data/Data.json", (data) => {
+    const dataContent = data.filter((data) => data.id == id);
+
+    $(`#${id}`).fadeOut(250, () => {
+      $(`#${id}`).replaceWith(
+        `<img
+    id="${id}"
+    onmouseover="blured(this.id,this.width,this.height)"
+    src="${dataContent[0].src}"
+    data-high-res-image-src="${dataContent[0].src}"
+    style="width:${dataContent[0].width}px; height:${dataContent[0].height}px"
+  /> `
+      );
+
+      $(`#${id}`).hide();
+      $(`#${id}`).fadeIn(250);
+
+      $("#myMosaic").Mosaic({
+        maxRows: 10,
+        maxRowHeightPolicy: "crop",
+        maxRowHeight: 500,
+      });
+    });
+  });
+}
+
+$("#myMosaic").Mosaic({
+  maxRows: 10,
+  maxRowHeightPolicy: "crop",
+  maxRowHeight: 500,
+});
+
+
+/*
+Div de layout
+
+<div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+  <h5>Ae caralho funcionou até agora</h5>
+  <p>puta merda já parou</p>
+</div>  
+*/
+
+//Elaaa paaartiuuuu e nuunca maais voltttooou SURTOS DE LUCAS
